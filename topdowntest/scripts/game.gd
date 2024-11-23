e5c2e835f7e5aefe3fff594ecb25089f4a4f97a9
SADOVSKV <3217cody@gmail.com>
2024-11-23 17:12:45 +0100

    better enemy spawning

--- topdowntest/scripts/game.gd
+++ topdowntest/scripts/game.gd
@@ -4,12 +4,11 @@ var enemy_scene = preload("res://scenes/enemy.tscn")
 
 func _on_spawn_timer_timeout() -> void:
 	var enemy = enemy_scene.instantiate()
-	enemy.transform = get_global_transform()
+	#enemy.transform = get_global_transform()
 	var player_pos = $Klara.position
-	var x = randi_range(100,300)*sign(randf()-0.5)
-	var y = randi_range(100,300)*sign(randf()-0.5)
-	enemy.position.x = player_pos.x+x
-	enemy.position.y = player_pos.y+y
+	var angle = randi_range(0,359)
+	var shift = randi_range(300,310)
+	enemy.position = $Klara.position+shift*Vector2.from_angle(deg_to_rad(angle))
 	get_tree().root.add_child(enemy)
 	
 	
